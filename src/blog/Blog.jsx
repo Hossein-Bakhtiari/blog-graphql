b6666a3bc7654d5d0102e33887a6ce6cd64c8a71
hossein <hosseinbakhtiari781@outlook.com>
2024-05-25 07:14:09 +0330

    get data from hygraph and made a responsive desgine for blogs section

--- src/blog/Blog.jsx
+++ src/blog/Blog.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useQuery } from "@apollo/client";
+import React from "react";
+import { GET_BLOGS_INFO } from "../graphql/queries";
+import { Grid } from "@mui/material";
+import CardEL from "../shared/CardEL";
 
 function Blog() {
+  const { loading, data, error } = useQuery(GET_BLOGS_INFO);
+
+  if (loading) return <h4>Loading...</h4>;
+
+  if (error) return <h4>Error...</h4>;
+
+  console.log(data);
   return (
-    <div>Blog</div>
-  )
+    <Grid container spacing={2}>
+      {data.posts.map((post) => (
+        <Grid item xs={12} sm={6} md={4} key={post.id}>
+          <CardEL {...post} />
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
 
-export default Blog
+export default Blog;
