2edd06d899a06595ef58bb7d0489b154d14f6330
hossein <hosseinbakhtiari781@outlook.com>
2024-05-25 18:44:52 +0330

    develope authors page

--- src/authors/AuthorPage.jsx
+++ src/authors/AuthorPage.jsx
@@ -2,10 +2,11 @@ import { useQuery } from "@apollo/client";
 import React from "react";
 import { GET_AUTHOR_INFO } from "../graphql/queries";
 import { useParams } from "react-router-dom";
+import { Avatar, Container, Grid, Typography } from "@mui/material";
 
 function AuthorPage() {
   const { slug } = useParams();
-  
+
   const { loading, data, error } = useQuery(GET_AUTHOR_INFO, {
     variables: { slug },
   });
@@ -13,7 +14,24 @@ function AuthorPage() {
 
   if (error) return <h4>Error...</h4>;
   console.log(data);
-  return <div>AuthorPage</div>;
+  return (
+    <Container maxWidth="lg" >
+      <Grid container mt={10}>
+        <Grid item xs={12} display="flex" flexDirection="column" alignItems="center">
+          <Avatar src={data.author.avatar.url} sx={{width:250 , height: 250}} />
+          <Typography component="h3" variant="h5" fontWeight={700} mt={4}>
+            {data.author.name}
+          </Typography>
+          <Typography component="p" variant="h5" color="text.secondary" mt={2}>
+            {data.author.field}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          {data.author.description.html}
+        </Grid>
+      </Grid>
+    </Container>
+  ); 
 }
 
 export default AuthorPage;
